6ece4cfbe33c9dd606da8644e645a59eca41cefc
sydnik99 <43049088+sydnik99@users.noreply.github.com>
2018-12-10 15:27:05 -0500

    Add files via upload

--- LMBL/script.js
+++ LMBL/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	console.log("ready!");
 
-
+$(".channel").hover(function(){
+	$(".tvscreen").removeClass().addClass("tvscreen");
+	$(".tvscreen").addClass($(this).attr("data-name") );
+});
 
 });
