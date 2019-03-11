757a692e319c6ce4c1c8db29b00d38fd4e986f42
sydnik99 <43049088+sydnik99@users.noreply.github.com>
2019-03-11 15:02:30 -0400

    Add files via upload

--- Backpack/script.js
+++ Backpack/script.js
@@ -3,32 +3,40 @@ console.log("hello")
 
 setTimeout(function(){
     $(".snooze").addClass("alarming"); 
-    $(".snooze2").addClass("alarming"); 
     $(".sleep").addClass("alarming");
-    $(".restart").addClass("alarming"); 
-    $(".restart2").addClass("alarming"); 
+    $(".restart").addClass("alarming");  
     $(".again").addClass("alarming");
-    $(".feminist").addClass("alarmmode");
+    var imgs=$(".fem, .dog img, .ave img, .mono img, .cherry img, .bee img");
+    imgs.each(function(){
+    		$(this).animate({"margin-left":20}, 250, function(){
+    		$(this).animate({"margin-left":-20}, 500, function(){
+    		$(this).animate({"margin-left":20}, 500, function(){
+    			$(this).animate({"margin-left":0}, 250, function(){
+    				$(this).animate({"margin-top":9000}, 500);
+	
+    			});	
+		    		});	
+		    	}); 
+		    });
 
-  }, 35000);
+		    });
+    
+  }, 30000);
 
   $(".sleep").click(function() { 
-    $(".snooze2").removeClass("alarming");
     $(".snooze").removeClass("alarming");
     $(".sleep").removeClass("alarming");
+    $(".restart").removeClass("alarming");
+    $(".again").removeClass("alarming");
   });
 
 
   $(".again").click(function() { 
     $(".restart").removeClass("alarming");
-    $(".restart2").removeClass("alarming");
     $(".again").removeClass("alarming");
+    $(".snooze").removeClass("alarming");
+    $(".sleep").removeClass("alarming");
   });
 
-
-
-/* how can i turn off the div class for sleep and again once i click the button to snooze or restart the alarm ? 
-how can i make the bag(w. patches) vibrate and the have the patches fall off as the alarmmode? ( they are already sepearte div sandwiches > see original set up in the css page)
-also ask about how i can make the two bag shapes that i have width% on them not responsize with the rest of the screen */
 
 }); 
