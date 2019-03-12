91da8884aa5b69840ba1da8b0898411f051d76db
sydnik99 <43049088+sydnik99@users.noreply.github.com>
2019-03-12 14:03:35 -0400

    Add files via upload

--- Backpack/script.js
+++ Backpack/script.js
@@ -8,11 +8,27 @@ setTimeout(function(){
     $(".again").addClass("alarming");
     var imgs=$(".fem, .dog img, .ave img, .mono img, .cherry img, .bee img");
     imgs.each(function(){
-    		$(this).animate({"margin-left":20}, 250, function(){
-    		$(this).animate({"margin-left":-20}, 500, function(){
-    		$(this).animate({"margin-left":20}, 500, function(){
-    			$(this).animate({"margin-left":0}, 250, function(){
-    				$(this).animate({"margin-top":9000}, 500);
+            var delay=0;
+            if($(this).parent().hasClass("dog")){
+                delay=200;
+            }
+            if($(this).parent().hasClass("ave")){
+                delay=400;
+            }
+            if($(this).parent().hasClass("mono")){
+                delay=600;
+            }
+            if($(this).parent().hasClass("cherry")){
+                delay=800;
+            }
+            if($(this).parent().hasClass("bee")){
+                delay=1000;
+            }
+    		$(this).delay(delay).animate({"margin-left":20}, 150, function(){
+    		$(this).animate({"margin-left":-20}, 300, function(){
+    		$(this).animate({"margin-left":20}, 300, function(){
+    			$(this).animate({"margin-left":0}, 150, function(){
+    				$(this).animate({"margin-top":9000}, 300);
 	
     			});	
 		    		});	
@@ -32,10 +48,7 @@ setTimeout(function(){
 
 
   $(".again").click(function() { 
-    $(".restart").removeClass("alarming");
-    $(".again").removeClass("alarming");
-    $(".snooze").removeClass("alarming");
-    $(".sleep").removeClass("alarming");
+    location.reload();
   });
 
 
