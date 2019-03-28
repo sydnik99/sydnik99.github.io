c1a2426df0a89296cfe1364cb0ffb905f7675468
sydnik99 <43049088+sydnik99@users.noreply.github.com>
2019-03-28 12:12:39 -0400

    Add files via upload

--- Beychella/Beychella/demo/demo/script.js
+++ Beychella/Beychella/demo/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:600, jumpHeight: 150});
+  mugen.initialize({attackRadius: 200, monsterHitRadius: 150, attackDuration:600, jumpHeight: 150, timeBetweenMonsters: 2700});
 });
