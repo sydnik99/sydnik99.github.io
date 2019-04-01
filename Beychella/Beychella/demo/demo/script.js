37be89ece0880a8b8e605e9e4962cedf5849a421
sydnik99 <43049088+sydnik99@users.noreply.github.com>
2019-04-01 12:17:10 -0400

    Add files via upload

--- Beychella/Beychella/demo/demo/script.js
+++ Beychella/Beychella/demo/demo/script.js
@@ -1,4 +1,23 @@
+var changeBackground = function(){
+	$(".background").addClass("special");
+};
+
+var restoreBackground = function(){
+	$(".background").removeClass("special");
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 200, monsterHitRadius: 150, attackDuration:600, jumpHeight: 150, timeBetweenMonsters: 2700});
+  mugen.initialize({attackRadius: 200, 
+  	monsterHitRadius: 150, 
+  	attackDuration:600, 
+  	jumpHeight: 150, 
+  	timeBetweenMonsters: 3000, 
+  	titleScreenImageURL:"./img/Beychella_Screen1.png", 
+  	monsterMoveToX: 15,
+  	maxSimultaneousMonsters:1,
+  	timeBetweenMonsters: 5000,
+  	makeResponsive: true,
+  	onMonsterSpawn: changeBackground, 
+  	onMonsterWasHit: restoreBackground });
 });
